5f500af5003625a2fb9d4020a6ea4e5ef9018ea0
Marko JOVANOVIC <komarjova75@gmail.com>
2026-03-22 12:46:40 +0100

    correction des bugs : getraceofunner +regle metier des emails

--- src/main/java/com/takima/race/registration/repositories/registrationRepository.java
+++ src/main/java/com/takima/race/registration/repositories/registrationRepository.java
@@ -10,6 +10,8 @@ import com.takima.race.registration.entities.Registration;
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
     Long countByRaceId(Long raceId);
-    List<Registration> findByRaceId(Long raceId);
+    List<Registration> findByRunnerId(Long runnerId);
+    List<Registration> findByRaceId(Long RaceId);
+
 
 }
